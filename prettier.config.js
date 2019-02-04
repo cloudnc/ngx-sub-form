3a55e0fb2a09ba370ff0b99d4b06b064b5425281
Maxime Robert <maxime.robert1992@gmail.com>
2019-02-04 10:50:41 +0000

    chore(prettier): setup

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+};

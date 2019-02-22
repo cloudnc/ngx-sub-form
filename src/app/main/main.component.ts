b3a1b818f0a7776037f540070c992f6e991a9de0
Maxime Robert <maxime.robert1992@gmail.com>
2019-02-22 11:23:13 +0000

    chore: remove sidenav

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(map(result => result.matches));
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-}
+export class MainComponent {}

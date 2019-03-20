25ba467a7e62190900dbc3ab7f3f7e9f0c102d34
Maxime Robert <maxime.robert1992@gmail.com>
2019-03-20 11:22:51 +0000

    fix(lib): throw error if no formControls or no formControlName and do not recreate the form group once destroyed (because of the getter)

--- projects/ngx-sub-form/src/lib/ngx-sub-form.component.ts
+++ projects/ngx-sub-form/src/lib/ngx-sub-form.component.ts
@@ -1,24 +1,26 @@
-import { Input, OnDestroy } from '@angular/core';
-import { AbstractControl, ControlValueAccessor, FormGroup, ValidationErrors, Validator } from '@angular/forms';
+import { Input, OnDestroy, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormGroup, ValidationErrors, Validator } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { delay, tap } from 'rxjs/operators';
 import { Controls, ControlsNames, getControlsNames } from './ngx-sub-form-utils';
 
+export class FormControlsRequiredError extends Error {
+  message = `Passing the "formControls" is required to have NgxSubForm working`;
+}
+
+export class FormControlNameRequiredError extends Error {
+  message = `Passing the "formControlName" is required to have NgxSubForm working`;
+}
+
 export abstract class NgxSubFormComponent<ControlInterface, FormInterface = ControlInterface>
-  implements ControlValueAccessor, Validator, OnDestroy {
+  implements ControlValueAccessor, Validator, OnInit, OnDestroy {
   protected abstract formControls: Controls<FormInterface>;
 
   public get formControlNames(): ControlsNames<FormInterface> {
     return getControlsNames(this.formControls);
   }
 
-  private fg: FormGroup | undefined = undefined;
-  public get formGroup(): FormGroup {
-    if (!this.fg) {
-      this.fg = new FormGroup(this.formControls);
-    }
-    return this.fg;
-  }
+  public formGroup: FormGroup | undefined = undefined;
 
   protected onChange: Function | undefined = undefined;
   protected onTouched: Function | undefined = undefined;
@@ -28,10 +30,26 @@ export abstract class NgxSubFormComponent<ControlInterface, FormInterface = Cont
   @Input()
   public formControlName: string | undefined = undefined;
 
-  public validate(ctrl: AbstractControl): ValidationErrors | null {
+  public ngOnInit(): void {
+    if (!this.formControls) {
+      throw new FormControlsRequiredError();
+    }
+
+    if (!this.formControlName) {
+      throw new FormControlNameRequiredError();
+    }
+
+    this.formGroup = new FormGroup(this.formControls);
+  }
+
+  public validate(): ValidationErrors | null {
     // @hack see below where defining this.formGroup to null
-    if (!this.formControlName || !this.formGroup || this.formGroup.valid || this.formGroup.pristine) {
+    if (!this.formGroup || this.formGroup.valid || this.formGroup.pristine) {
       return null;
+    }
+
+    if (!this.formControlName) {
+      throw new FormControlNameRequiredError();
     }
 
     return { [this.formControlName]: true };
@@ -41,7 +59,7 @@ export abstract class NgxSubFormComponent<ControlInterface, FormInterface = Cont
     // @hack there's a memory leak within Angular and those components
     // are not correctly cleaned up which leads to error if a form is defined
     // with validators and then it's been removed, the validator would still fail
-    this.fg = undefined;
+    this.formGroup = undefined;
 
     if (this.subscription) {
       this.subscription.unsubscribe();
@@ -50,10 +68,13 @@ export abstract class NgxSubFormComponent<ControlInterface, FormInterface = Cont
     if (this.onChange) {
       this.onChange(null);
     }
+
+    this.onChange = undefined;
   }
 
-  public writeValue(obj: any): void {
-    if (obj) {
+  public writeValue(obj: Required<ControlInterface>): void {
+    // should accept falsy values like `false` or empty string
+    if (obj !== null && obj !== undefined) {
       if (!!this.formGroup) {
         this.formGroup.setValue(this.transformToFormGroup(obj), {
           emitEvent: false,
@@ -82,6 +103,10 @@ export abstract class NgxSubFormComponent<ControlInterface, FormInterface = Cont
   }
 
   public registerOnChange(fn: (_: any) => void): void {
+    if (!this.formGroup) {
+      return;
+    }
+
     this.onChange = fn;
 
     // this is required to correctly initialize the form value

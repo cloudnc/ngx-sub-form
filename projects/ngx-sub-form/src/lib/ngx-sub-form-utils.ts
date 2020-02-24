c1afcdd92e7f055a98c099e9fb3e81bc5efe96a6
ntziolis <nikolas@tziolis.de>
2020-02-24 21:48:34 +0100

    feat(typings): use optional typing for patch/setValue on TypedAbstractControl

--- projects/ngx-sub-form/src/lib/ngx-sub-form-utils.ts
+++ projects/ngx-sub-form/src/lib/ngx-sub-form-utils.ts
@@ -39,8 +39,14 @@ export type FormErrors<FormInterface> = null | Partial<
 export interface TypedAbstractControl<TValue> extends AbstractControl {
   value: TValue;
   valueChanges: Observable<TValue>;
-  setValue(value: TValue, options?: Parameters<AbstractControl['setValue']>[1]): void;
-  patchValue(value: Partial<TValue>, options?: Parameters<AbstractControl['patchValue']>[1]): void;
+  setValue(
+    value: TValue,
+    options?: Parameters<FormControl['setValue']>[1] | Parameters<FormGroup['setValue']>[1],
+  ): void;
+  patchValue(
+    value: Partial<TValue>,
+    options?: Parameters<FormControl['patchValue']>[1] | Parameters<FormGroup['patchValue']>[1],
+  ): void;
 }
 
 export interface TypedFormGroup<TValue> extends FormGroup {

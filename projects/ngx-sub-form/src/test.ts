74cbc0c0d5de3dd0b2af8094f1e79950a2d3b4e8
Maxime Robert <maxime.robert1992@gmail.com>
2021-11-18 16:34:21 +0100

    chore: update angular deps to version 13.0.2

--- projects/ngx-sub-form/src/test.ts
+++ projects/ngx-sub-form/src/test.ts
@@ -1,15 +1,17 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'core-js/proposals/reflect-metadata';
 import 'zone.js';
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: false },
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.

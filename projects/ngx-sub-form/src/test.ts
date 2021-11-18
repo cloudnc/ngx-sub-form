ffbc52a1932b8b87c0dc13e46c46388c4716eec7
Maxime Robert <maxime.robert1992@gmail.com>
2021-11-18 16:17:37 +0100

    chore: update angular deps to version 12.2.13

--- projects/ngx-sub-form/src/test.ts
+++ projects/ngx-sub-form/src/test.ts
@@ -1,8 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'core-js/proposals/reflect-metadata';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+import 'zone.js';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 

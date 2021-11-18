d6d3d64f7ae092b779e19476c4ea594180b3d757
Maxime Robert <maxime.robert1992@gmail.com>
2021-11-18 16:49:04 +0100

    test: fix import reordered by IDE and breaking the tests

--- projects/ngx-sub-form/src/test.ts
+++ projects/ngx-sub-form/src/test.ts
@@ -1,10 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'zone.js';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'core-js/proposals/reflect-metadata';
-import 'zone.js';
-import 'zone.js/testing';
 
 declare const require: any;
 
